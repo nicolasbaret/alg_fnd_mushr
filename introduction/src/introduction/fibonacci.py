129dc895521607b9238a424419a4f2f3ee4f879c
robotics <nickbaret5@gmail.com>
2025-08-24 09:29:08 -0700

    init - and add fibo implementation

--- introduction/src/introduction/fibonacci.py
+++ introduction/src/introduction/fibonacci.py
@@ -13,5 +13,13 @@ def compute_fibonacci(n):
     3
     """
     # BEGIN QUESTION 1.1
-    "*** REPLACE THIS LINE ***"
+    if n == 0: return 0
+    if n == 1: return 1
+    else:
+        arr = [0] * (n + 1)
+        arr[0] = 0
+        arr[1] = 1
+        for i in range(2,n+1):
+            arr[i] = arr[i-1] + arr[i-2]
+        return arr[n]
     # END QUESTION 1.1
